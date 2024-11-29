37cd3ef6c9a9e36049269cfe499b11c2a64affa1
rod-sidney <rodriguezcarr.sidney@gmail.com>
2024-11-29 17:38:42 -0600

    eliminar carpeta con documentos redundantes

--- src/rutas/rutasAdmins.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controladorAdmins = require('../controladores/controladorAdmins'); // Importa el controlador
-
-// Ruta para crear un administrador
-router.post('/', controladorAdmins.crearAdmin);
-
-module.exports = router;
