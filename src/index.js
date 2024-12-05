e976bfca1bce209435bd5cde13399a392d67a569
Rayner Rod <rrodriguezs@ucenfotec.ac.cr>
2024-12-05 01:32:19 -0600

    arreglo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 
 const app = express();
-const cors = require('cors');
-const path = require('path');
 
 const path = require('path'); //unifica elementos
 app.set('views', path.join(__dirname, 'views'));
